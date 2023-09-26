bc5fc74ee0b586a3e5abb78ba10b28c3ca0f6d54
rajat123yadav <100460887+rajat123yadav@users.noreply.github.com>
2023-09-26 17:09:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1781,13 +1781,13 @@ elif selected_option_case_type == "AML":
                     query = "What are the transaction that can be associated with Money Laundering activity?"
                     context_1 = docsearch.similarity_search(query, k=5)
                     prompt_1 =  f'''You Are an Anti-Money Laundering Specialist, Identify the transactions \
-                                that can be potentially associated with the Money Laundering activity from Credit Card transaction statement only. \n
+                                that can be potentially associated with the Money Laundering activity both from Credit Card transaction statement as well as savings account statement collectively. \n
                                 Money laundering transactions often involve characteristics like large cash deposits greater than or equal to $10,000 \
                                 Payments greater than or equal to 10000$ to an unrecognized entity with no specific  business purpose, \ 
-                                , transactions involving movement of funds to or from high-risk locations (Ex- Mauritious, Syria, Nigeria,etc.)and are greater than or equal to 10000$, any suspicion of money laundered via structuring , layering or intergration, process, \
-                                Cash deposits greater than or eaual to 10000$ with source of funds not clear used to pay off debt, etc. \n
+                                , transactions involving movement of funds to or from high-risk locations(Ex- Mauritious, Syria, Nigeria,etc.), any suspicion of money laundered via structuring , layering or intergration, process, \
+                                Cash deposits with source of funds not clear used to pay off debt, etc. \n
                                 Do not consider transactions less than 10000$ for evaluation. \n
-                                Give all such suspicious transactions along with dates and amounts and description from the context as your response \
+                                Describe all such suspicious transactions grouped by transaction type(Credit card, savings account,etc.) along with dates, amounts and description from the context as your response \
                                 Do not repeat the above information and provide a to the point response. Also, do not include transactions less than 10000$ in your response.\n\n
                                 Context: {context_1}\n\
                                 Response: (Give me a concise response .Do not give me any Explanation,Note, etc.)'''
